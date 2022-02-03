b3d5dabceeef49fe3d1a5bf350d159d4088ba319
Ea-Bell <cjp975@naver.com>
2022-02-04 00:42:42 +0900

    예제 도메인 모델만듬

--- data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -1,24 +1,40 @@
 package study.datajpa.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)    //JPA는 기본 생성자가 있어야 한다. protected를 설정하면 아무데서나 불러올수 없게 만들기 위해서
+@ToString(of = {"id", "username", "age"})
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
 
-    //JPA는 기본 생성자가 있어야 한다. protected를 설정하면 아무데서나 불러올수 없게 만들기 위해서
-    protected Member(){}
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String username) {
         this.username = username;
     }
+
+    public Member(String username, int age, Team team) {
+        this.username=username;
+        this.age=age;
+        if(team != null){
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team=team;
+        team.getMembers().add(this);
+    }
+
 }
