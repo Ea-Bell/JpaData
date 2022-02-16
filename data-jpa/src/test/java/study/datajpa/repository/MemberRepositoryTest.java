2ba0aea5b80f5a104aba9c574ac09008dc351201
Ea-Bell <cjp975@naver.com>
2022-02-16 11:00:13 +0900

    쿼리 메소드 기능
    @Query, 값, DTO조회하기
    
    주의!DTO로 직접 조회 하려면 JPA의 new 명령어를 사용해야 한다. 그리고 다음과 같이 생성자가 맞는 DTO가 필요하다(JPA와 사용방식이 동일하다.)

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,6 +23,7 @@ class MemberRepositoryTest {
 
     @Autowired MemberRepository memberRepository;
     @Autowired MemberJpaRepository memberJpaRepository;
+    @Autowired TeamRepository teamRepository;
 
 
     @Test
@@ -127,4 +130,36 @@ class MemberRepositoryTest {
         assertThat(result.get(0)).isEqualTo(m1);
     }
 
-}
+    @Test
+    public void testFindUsernameList(){
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<String> usernameList = memberRepository.findUsernameList();
+        for (String member : usernameList) {
+            //System.out.println("member = " + member);
+            assertThat(usernameList).contains(member);
+        }
+
+        }
+    @Test
+    public void findMEmberDto() {
+        Team team= new Team("teamA");
+        teamRepository.save(team);
+
+        Member m1 = new Member("AAA", 10);
+        m1.setTeam(team);
+        memberRepository.save(m1);
+
+        List<MemberDto> memberDto = memberRepository.findMemberDto();
+        for (MemberDto dto : memberDto) {
+            System.out.println("dto = " + dto);
+        }
+
+    }
+
+
+    }
+
